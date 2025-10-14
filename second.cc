83f880d8a50d36f946e09ec9fd40c4ba27dec444
Juan Lobo <113059377+ojuanlp@users.noreply.github.com>
2025-10-14 10:04:41 -0300

    Update second.cc

--- second.cc
+++ second.cc
@@ -1,17 +1,8 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Este programa é baseado em second.cc e modificado para lab1-part2.cc.
+ * Resolve a Parte 2 do laboratório, adicionando um segundo link P2P
+ * ao nó do servidor e configurando a simulação para análise de atraso.
  */
 
 #include "ns3/core-module.h"
@@ -21,94 +12,160 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/netanim-module.h" // Incluído se desejar visualização (não obrigatório)
 
-// Default Network Topology
+// Nova Topologia:
 //
-//       10.1.1.0
-// n0 -------------- n1   n2   n3   n4
-//    point-to-point  |    |    |    |
+//       10.1.1.0           10.1.3.0
+// n0 (Client) --- n1 | n2 | n3 | n4 --- n5 (Server)
+//    point-to-point  |    |    |    |  point-to-point
 //                    ================
 //                      LAN 10.1.2.0
 
-
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("SecondScriptExample");
+NS_LOG_COMPONENT_DEFINE ("Lab1Part2Script");
 
-int 
+int
 main (int argc, char *argv[])
 {
-  bool verbose = true;
-  uint32_t nCsma = 3;
+  // Parâmetros de Linha de Comando
+  uint32_t nCsma = 3;    // Número de nós "extras" CSMA (n2, n3, n4 no exemplo)
+  uint32_t nClients = 1; // nClients para compatibilidade com o enunciado (n0 é o único cliente)
+  uint32_t nPackets = 20; // Padrão: 20 pacotes, conforme o enunciado para análise
 
   CommandLine cmd (__FILE__);
-  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
-  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices (n2, n3, ...). Default is 3.", nCsma);
+  cmd.AddValue ("nPackets", "Number of packets for UdpEchoClient to send. Default is 20.", nPackets);
+  // O enunciado pede nClients, mas a topologia tem apenas um cliente (n0). Usamos nClients=1 como valor fixo.
+  // cmd.AddValue ("nClients", "Number of clients (fixed at 1 for this topology).", nClients); // Mantido o default
 
   cmd.Parse (argc,argv);
 
-  if (verbose)
-    {
-      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    }
+  // O enunciado original usa o exemplo de 4 nós CSMA (n1, n2, n3, n4), onde n1 é compartilhado com P2P.
+  // O número de "extra" CSMA é nCsma. Se nCsma=3, temos 4 nós CSMA no total (n1 + 3 extras).
+  // A topologia de exemplo mostra 5 nós CSMA (n1, n2, n3, n4, n5) onde n5 é o servidor.
+  // Vamos usar nCsma = 4 para ter n1, n2, n3, n4, com n4 sendo o último nó na LAN.
+  // Para 4 CSMA nodes (3 "extra" nodes), o nCsma deve ser 3.
 
+  // O número total de nós na LAN CSMA é nCsma + 1 (onde +1 é o nó n1)
+  // O servidor n5 será um nó separado, criando a nova topologia.
+  
+  // Garantir que nCsma seja pelo menos 1
   nCsma = nCsma == 0 ? 1 : nCsma;
 
-  NodeContainer p2pNodes;
-  p2pNodes.Create (2);
+  // 1. Criação dos Nós
+  NodeContainer p2pNodes; // Contém n0 e n1
+  p2pNodes.Create (2); // n0 (Cliente) e n1
 
   NodeContainer csmaNodes;
-  csmaNodes.Add (p2pNodes.Get (1));
-  csmaNodes.Create (nCsma);
+  csmaNodes.Add (p2pNodes.Get (1)); // Adiciona n1 (compartilhado)
+  csmaNodes.Create (nCsma); // Adiciona n2, n3, n4 (se nCsma=3)
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  Ptr<Node> n0 = p2pNodes.Get (0); // Cliente
+  Ptr<Node> n4 = csmaNodes.Get (nCsma); // Último nó na LAN (n4 no exemplo)
+  
+  NodeContainer serverNode;
+  serverNode.Create (1); // n5 (Servidor)
+  Ptr<Node> n5 = serverNode.Get (0); // Servidor
+  
+  // 2. Helpers e Configuração de Links
 
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
+  // Link P2P Original (n0 --- n1)
+  PointToPointHelper pointToPoint1;
+  pointToPoint1.SetDeviceAttribute ("DataRate", StringValue ("5Mbps")); // 5 Mbps (Original)
+  pointToPoint1.SetChannelAttribute ("Delay", StringValue ("2ms"));    // 2 ms (Original)
 
+  // Link P2P Modificado (n4 --- n5) - Deve ter os MESMOS parâmetros (Nota importante)
+  PointToPointHelper pointToPoint2;
+  pointToPoint2.SetDeviceAttribute ("DataRate", StringValue ("5Mbps")); // 5 Mbps (Novo P2P)
+  pointToPoint2.SetChannelAttribute ("Delay", StringValue ("2ms"));    // 2 ms (Novo P2P)
+
+  // Link CSMA (LAN 10.1.2.0)
   CsmaHelper csma;
   csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
-  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560))); // 6.56 us
 
+  // 3. Instalação dos Dispositivos
+
+  // P2P 1: n0 --- n1
+  NetDeviceContainer p2p1Devices;
+  p2p1Devices = pointToPoint1.Install (p2pNodes);
+
+  // CSMA: n1 | n2 | n3 | n4
   NetDeviceContainer csmaDevices;
   csmaDevices = csma.Install (csmaNodes);
 
-  InternetStackHelper stack;
-  stack.Install (p2pNodes.Get (0));
-  stack.Install (csmaNodes);
+  // P2P 2: n4 --- n5 (Último nó da LAN CSMA e Servidor)
+  NodeContainer p2p2Nodes = NodeContainer (n4, n5);
+  NetDeviceContainer p2p2Devices;
+  p2p2Devices = pointToPoint2.Install (p2p2Nodes);
 
+  // 4. Instalação da Stack de Protocolos
+  InternetStackHelper stack;
+  stack.Install (p2pNodes.Get (0)); // n0
+  stack.Install (csmaNodes);        // n1, n2, n3, n4
+  stack.Install (serverNode);       // n5
+
+  // 5. Configuração de Endereços IP
+
+  // 10.1.1.0: n0 (Client) --- n1
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces;
-  p2pInterfaces = address.Assign (p2pDevices);
+  Ipv4InterfaceContainer p2p1Interfaces;
+  p2p1Interfaces = address.Assign (p2p1Devices);
 
+  // 10.1.2.0: LAN CSMA
   address.SetBase ("10.1.2.0", "255.255.255.0");
   Ipv4InterfaceContainer csmaInterfaces;
   csmaInterfaces = address.Assign (csmaDevices);
 
-  UdpEchoServerHelper echoServer (9);
+  // 10.1.3.0: n4 --- n5 (Server)
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer p2p2Interfaces;
+  p2p2Interfaces = address.Assign (p2p2Devices);
+  
+  // Endereço do Servidor (n5) para o Cliente (n0)
+  Ipv4Address serverAddress = p2p2Interfaces.GetAddress (1); // n5 é o segundo nó do p2p2, IP .2
+  uint16_t serverPort = 9;
 
-  ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
+  // 6. Configuração do Roteamento
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  // 7. Configuração da Aplicação Servidora (UdpEchoServer)
+  // O Servidor é n5
+  UdpEchoServerHelper echoServer (serverPort);
+
+  ApplicationContainer serverApps = echoServer.Install (n5);
   serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
+  serverApps.Stop (Seconds (20.0));
 
-  UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  // 8. Configuração da Aplicação Cliente (UdpEchoClient)
+  // O Cliente é n0
+  UdpEchoClientHelper echoClient (serverAddress, serverPort);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets)); // Usar nPackets (default 20)
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
+  ApplicationContainer clientApps = echoClient.Install (n0);
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  clientApps.Stop (Seconds (20.0)); // Garantir que pare em 20.0s (conforme a nota importante)
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  // 9. Captura de Pacotes (PCAP)
+  
+  // Capturas solicitadas (mínimo 3):
+  // 1. O P2P original ("second")
+  pointToPoint1.EnablePcapAll ("lab1-part2-p2p1");
+  // 2. O novo P2P ("p2p2")
+  pointToPoint2.EnablePcapAll ("lab1-part2-p2p2");
+  // 3. A LAN CSMA (n1, dispositivo 0 no container CSMA)
+  csma.EnablePcap ("lab1-part2-csma", csmaDevices.Get (0), true);
+  
+  // Para seguir o original second.cc, a captura de todos os dispositivos na LAN
+  // csma.EnablePcapAll ("lab1-part2-csma-all");
 
-  pointToPoint.EnablePcapAll ("second");
-  csma.EnablePcap ("second", csmaDevices.Get (1), true);
-
+  // 10. Execução
+  Simulator::Stop (Seconds (20.0));
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;
